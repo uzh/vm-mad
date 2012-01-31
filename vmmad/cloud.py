ca48397591e1e75f6836c80058d3a68b253aa792
riccardo.murri <riccardo.murri@c1fca213-d77e-5eb5-9d24-8ba6f8ca3605>
2012-01-31 22:40:30 +0000

    Skeletal code for the EC2 cloud provider.
      modified:   cloud.py

--- vmmad/cloud.py
+++ vmmad/cloud.py
@@ -114,6 +114,9 @@ class Cloud:
 
 
 class DummyCloud(Cloud):
+    """
+    Interface `Apache LibCloud <http://libcloud.apache.org/>` "dummy" cloud provider.
+    """
 
     def __init__(self, image='1', kind='1'):
 
@@ -172,6 +175,83 @@ class DummyCloud(Cloud):
         nodes = [ node for node in self.provider.list_nodes()
                   if node.id in self._instance_to_vm_map ]
         for node in nodes:
-            self._instance_to_vm_map[node.uuid].instance = node
-            self._instance_to_vm_map[node.uuid].state = node.state
+            vm = self._instance_to_vm_map[node.uuid]
+            vm.instance = node
+            vm.state = self._vminfo_state_from_libcloud_status(node.state)
 
+
+
+class EC2Cloud(Cloud):
+    """
+    Interface to Amazon EC2 on top of `Apache LibCloud <http://libcloud.apache.org/>`.
+    """
+
+    def __init__(self, image, kind, access_id=None, secret_key=None):
+
+        self.image = image
+        self.kind = kind
+        if access_id is not None:
+            self.access_id = access_id
+            self.secret_key = secret_key
+        else:
+            # use same environment variables as Boto
+            self.access_id = os.environ['AWS_ACCESS_KEY_ID']
+            self.secret_key = os.environ['AWS_SECRET_ACCESS_KEY']
+
+        log.debug("Creating EC2 cloud provider with access ID '%s' ...", access_id)
+        driver = libcloud.compute.providers.get_driver(libcloud.compute.types.Provider.EC2)
+        self.provider = driver(self.access_id, self.secret_key)
+        log.info("Using cloud provider '%s'.", self.provider.friendly_name)
+
+        log.info("Listing available images ...")
+        self._images = dict((img.id, img) for img in self.provider.list_images())
+        if image not in self._images:
+            raise RuntimeError("Image '%s' not available on %s"
+                               % (image, self.provider.friendly_name))
+        log.info("... done: %d images available.", len(self._images))
+
+        log.info("Listing available sizes ...")
+        self._kinds = dict((kind.id, kind) for kind in self.provider.list_sizes())
+        if kind not in self._kinds:
+            raise RuntimeError("Kind '%s' not available on %s"
+                               % (kind, self.provider.friendly_name))
+        log.info("... done: %d kinds available.", len(self._kinds))
+
+        log.info("VMs will use image '%s' (%s) on hardware kind '%s' (%s)",
+                 self.image, self._images[self.image].name,
+                 self.kind, self._kinds[self.kind].name)
+
+        # log.info("Getting list of running instances ...")
+        # self.instances = dict((node.uuid, node) for node in self.provider.list_nodes())
+        # log.info("... Done: %d instances available.", len(self._sizes))
+
+        # associate the Node ID we get from the cloud provider with
+        # the VM object we get from the orchestrator
+        self._instance_to_vm_map = { }
+        
+
+    def start_vm(self, vm):
+        vm.instance = self.provider.create_node(
+            name=str(vm.vmid), image=self._images[self.image], size=self._kinds[self.kind])
+        vm.cloud = self.provider
+        self._instance_to_vm_map[vm.instance.uuid] = vm
+
+
+    def stop_vm(self, vm):
+        # XXX: this is tricky: we must:
+        #   1. gracefully shutdown the node, and (after a timeout) proceed to:
+        #   2. destroy the node
+        # In addition this should not block the main Orchestrator thread.
+        id = vm.instance.uuid
+        self.provider.destroy_node(vm.instance)
+        del self._instance_to_vm_map[id]
+
+
+    def update_vm_status(self, vms):
+        nodes = self.provider.list_nodes(ex_node_ids=[vm.instance.id for vm in vms])
+        for node in nodes:
+            vm = self._instance_to_vm_map[node.uuid]
+            vm.instance = node
+            vm.state = self._vminfo_state_from_libcloud_status(node.state)
+            
+            
