59358a9a69021affb77ac5218b282f8654ae3b82
tyanko.alexiev@gmail.com <tyanko.alexiev@gmail.com@c1fca213-d77e-5eb5-9d24-8ba6f8ca3605>
2012-02-16 16:20:05 +0000

    Simulator bugs fixed:
    1. KeyError
    2. job_time_interval and time_interval unified
    3. negative number of idleVMs

--- vmmad/simul.py
+++ vmmad/simul.py
@@ -43,11 +43,10 @@ from vmmad import log
 import cloud
 from orchestrator import Orchestrator, JobInfo, VmInfo
 
-
 class OrchestratorSimulation(Orchestrator, cloud.DummyCloud):
 
     def __init__(self, max_vms, max_delta, max_idle, startup_delay,
-                 job_number, min_duration, max_duration, output_file, csv_file, start_time, time_interval, job_time_interval, cluster_size):
+                 job_number, min_duration, max_duration, output_file, csv_file, start_time, time_interval, cluster_size):
         # implement the `Cloud` interface to simulate a cloud provider
         cloud.DummyCloud.__init__(self, '1', '1')
 
@@ -60,24 +59,24 @@ class OrchestratorSimulation(Orchestrator, cloud.DummyCloud):
         self.sched_time = int(mktime(dt.timetuple()))
 	
         # Set simulation settings
-        self.csv_file = csv_file
         self.max_idle = max_idle
         self.startup_delay = startup_delay
         self.output_file = output_file
         self.cluster_size = int(cluster_size) 
         self.time_interval = int(time_interval)
-        self.job_time_interval = int(job_time_interval)
+        self.job_time_interval = int(time_interval)
+        self._next_row = None
 
         # info about running VMs
         self._vmid = 0 
         self._idle_vm_count = 0
         self._steps = 0
-       
-        # start "cluster_size" of jobs in order to "wake-up" the real hardware of our disposal 
+      
         self._running = [ ]
-
         self._pending = [ JobInfo(jobid=1, state=JobInfo.PENDING, duration=0) ]
 
+        self.input_file = open(csv_file, 'r')
+        self.csv_file = csv.reader(self.input_file, delimiter=' ')
 
     def update_job_status(self):
         # do regular work	
@@ -102,7 +101,7 @@ class OrchestratorSimulation(Orchestrator, cloud.DummyCloud):
                 vm.jobs.add(job.jobid)
                 job.vm = vm
                 vm.last_idle = 0
-                if not vm.ever_running:
+                if not vm.ever_running: 
                     if self._idle_vm_count > 0:
                         self._idle_vm_count -= 1
                 self._running.append(job)
@@ -110,18 +109,18 @@ class OrchestratorSimulation(Orchestrator, cloud.DummyCloud):
 
     def before(self):
         self._steps += 1
-        if len(self._pending) == 0 and len(self._running) == 0:
-            log.info("No more jobs, stopping simulation.")
+        if len(self._running) == 0 and len(self._pending) == 0 and self.csv_file is None:
+            log.info("No more jobs to read from file '%s', stopping here", self.input_file.name)
             sys.exit(0)
-        
+            
         with open((self.output_file), "a") as output:
             output.write(
                 "%s,%s,%s,%s,%s\n"
                 #  no. of steps,      pending jobs,       running jobs,            started VMs,            idle VMs,
                 %(self._steps, len(self._pending), len(self._running), len(self._started_vms), self._idle_vm_count))
 
-        log.info("At step %d: pending jobs %d, running jobs %d, started VMs %d, idle VMs %d",
-                     self._steps, len(self._pending), len(self._running), len(self._started_vms), self._idle_vm_count)
+        log.info("At time %d: pending jobs %d, running jobs %d, started VMs %d, idle VMs %d",
+                     (self.sched_time + self.time_interval*self._steps), len(self._pending), len(self._running), len(self._started_vms), self._idle_vm_count)
 
 
     ##
@@ -131,11 +130,30 @@ class OrchestratorSimulation(Orchestrator, cloud.DummyCloud):
         time_step =  Orchestrator.get_time_step(self)
         previous_time = self.sched_time + (time_step-1)*self.time_interval
         new_time= self.sched_time + time_step*self.time_interval
-        reader = csv.reader(open(self.csv_file), delimiter=' ')
-        for row in reader:
-    	    if int(row[1]) > previous_time and int(row[1]) <= new_time:
-	            self._pending.append(JobInfo( jobid=int(row[0]), state=JobInfo.PENDING, duration=int(row[2])) )		
+        for job in self.read_next_jobs(previous_time, new_time): 
+            self._pending.append(job)
         return (self._running + self._pending)
+
+    def read_next_jobs(self, since, until):
+        while self.csv_file is not None:
+            try:    
+                row = self._next_row or self.csv_file.next()
+            except StopIteration:
+                self.csv_file = None
+                raise
+            submit_time = int(row[1])
+            if since > submit_time:
+                if since <= until:
+                    self._next_row = None
+                    yield JobInfo(jobid=int(row[0]), state=JobInfo.PENDING, duration=int(row[2]))
+                else:
+                    # job is in the past (relative to the Simulator's conecpt of time) so ignore it
+                    pass
+            else:
+                # job is in the future: stop here and use it next time around
+                self._next_row = row
+                raise StopIteration
+
 
     ##
     ## policy implementation interface
@@ -159,9 +177,9 @@ class OrchestratorSimulation(Orchestrator, cloud.DummyCloud):
 
     def start_vm(self, vm):
         cloud.DummyCloud.start_vm(self, vm)
+        log.info("Started VM %d (%s).", vm.vmid, vm.instance.uuid)
         vm.been_running=0
         vm.total_idle=0
-        vm.last_idle=(-self.startup_delay)
 
         # Setting the first "cluster_size" machines to be ever_running. 
         if vm.vmid <= self.cluster_size:
@@ -169,24 +187,25 @@ class OrchestratorSimulation(Orchestrator, cloud.DummyCloud):
             vm.last_idle = 1 
         else:
             vm.ever_running = False 
+            vm.last_idle=(-self.startup_delay)
 
     def update_vm_status(self, vms):
         cloud.DummyCloud.update_vm_status(self, vms)
         for vm in self._started_vms:
             if not vm.ever_running:
-                vm.been_running += 1
+                vm.been_running += self.time_interval
                 if not vm.jobs:
-                    vm.total_idle += 1
-                    vm.last_idle += 1
+                    vm.total_idle += self.time_interval
+                    vm.last_idle += self.time_interval
             else:
                 if not vm.jobs:
                     vm.last_idle = 1  
 
     def stop_vm(self, vm):
-        log.info("Stopping VM %s: has run for %d steps, been idle for %d of them",
-                     vm.vmid, vm.been_running, vm.total_idle)
+        log.info("Stopping VM %s (%s): has run for %d steps, been idle for %d of them",
+                     vm.vmid, vm.instance.uuid, vm.been_running, vm.total_idle)
         cloud.DummyCloud.stop_vm(self, vm)
-        if vm.last_idle > 0 and not vm.ever_running:
+        if vm.last_idle > 0 and self._idle_vm_count > 0 and not vm.ever_running:
             self._idle_vm_count -= 1
         return True
 
@@ -196,18 +215,17 @@ if "__main__" == __name__:
     parser = argparse.ArgumentParser(description='Simulates a cloud orchestrator')
     parser.add_argument('--max-vms', '-mv', metavar='N', dest="max_vms", default=10, type=int, help="Maximum number of VMs to be started, default is %(default)s")
     parser.add_argument('--max-delta', '-md', metavar='N', dest="max_delta", default=1, type=int, help="To be defined")    
-    parser.add_argument('--max-idle', '-mi', metavar='NUM_SECS', dest="max_idle", default=30, type=int, help="Maximum idle time (in seconds) before swithing off a VM, default is %(default)s")
-    parser.add_argument('--startup-delay', '-S', metavar='NUM_SECS', dest="startup_delay", default=60, type=int, help="Time (in seconds) delay before staring up a VM, default is %(default)s")
+    parser.add_argument('--max-idle', '-mi', metavar='NUM_SECS', dest="max_idle", default=7200, type=int, help="Maximum idle time (in seconds) before swithing off a VM, default is %(default)s")
+    parser.add_argument('--startup-delay', '-S', metavar='NUM_SECS', dest="startup_delay", default=3600, type=int, help="Time (in seconds) delay before staring up a VM, default is %(default)s")
     parser.add_argument('--job-number', '-jn', metavar='N', dest="job_number", default=50, type=int, help="Number of job to be started, default is %(default)s")
     parser.add_argument('--min-duration', '-mind', metavar='NUM_SECS', dest="min_duration", default=30, type=int, help="Lower bound for job's time (in seconds) execution, default is %(default)s")
     parser.add_argument('--max-duration', '-maxd', metavar='NUM_SECS', dest="max_duration", default=120, type=int, help="Upper bound for job's time (in seconds)  execution, default is %(default)s")
     parser.add_argument('--csv-file', '-csvf',  metavar='String', dest="csv_file", default="output.csv", help="File containing the CSV information, %(default)s")
     parser.add_argument('--output-file', '-O',  metavar='String', dest="output_file", default="main_sim.txt", help="File name where the output of the simulation will be stored, %(default)s") 
-    parser.add_argument('--job-time-interval', '-jtimei',  metavar='NUM_SECS', dest="job_time_interval", default="300", help="Time interval to be substracted from job duration during iterations, default: %(default)s")
     parser.add_argument('--cluster-size', '-cs',  metavar='NUM_CPUS', dest="cluster_size", default="20", help="Number of VMs, used for the simulation of real available cluster: %(default)s")
     parser.add_argument('--start-time', '-stime',  metavar='String', dest="start_time", default="1970-01-01T00:00:00", help="Start time for the simulation, default: %(default)s")
     parser.add_argument('--time-interval', '-timei',  metavar='NUM_SECS', dest="time_interval", default="3600", help="UNIX interval in seconds used as parsing interval for the jobs in the CSV file, default: %(default)s")
     parser.add_argument('--version', '-V', action='version',
                         version=("%(prog)s version " + __version__))
     args = parser.parse_args()
-    OrchestratorSimulation(args.max_vms, args.max_delta, args.max_idle, args.startup_delay, args.job_number, args.min_duration, args.max_duration, args.output_file, args.csv_file, args.start_time, args.time_interval, args.job_time_interval, args.cluster_size).run(0)
+    OrchestratorSimulation(args.max_vms, args.max_delta, args.max_idle, args.startup_delay, args.job_number, args.min_duration, args.max_duration, args.output_file, args.csv_file, args.start_time, args.time_interval, args.cluster_size).run(0)
