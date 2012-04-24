652fbfec849cc35d9000f0d07af9f56ba2bf49d4
riccardo.murri <riccardo.murri@c1fca213-d77e-5eb5-9d24-8ba6f8ca3605>
2012-04-24 12:25:44 +0000

    batchsys/gridengine.py: Make indentation consistent with other files.
    Use 4 spaces for each indent level, and never ever use TABs.

--- vmmad/batchsys/gridengine.py
+++ vmmad/batchsys/gridengine.py
@@ -44,120 +44,120 @@ from vmmad.util import Struct
 
 
 class _QstatXmlHandler(xml.sax.ContentHandler):
-        """
-        SAX `ContentHandler` implementation for parsing the output of
-        `qstat -u ... -xml`.
-        """
+    """
+    SAX `ContentHandler` implementation for parsing the output of
+    `qstat -u ... -xml`.
+    """
 
-        # XML attributes *not* in this list are ignored
-        JOB_ATTRIBUTES = [
-                'JB_job_number',
-                'JB_submission_time',
-                'JAT_start_time',
-                'JAT_prio',
-                'JB_name',
-                'state',
-                'queue_name',
-                'slots'
-                ]
+    # XML attributes *not* in this list are ignored
+    JOB_ATTRIBUTES = [
+        'JB_job_number',
+        'JB_submission_time',
+        'JAT_start_time',
+        'JAT_prio',
+        'JB_name',
+        'state',
+        'queue_name',
+        'slots'
+        ]
 
-        # conversion of XML fields to Python data
-        CONVERT = defaultdict(
-                # by default, return `str`
-                lambda: str,
-                # return other values in particular cases:
-                JB_job_number=int,
-                JB_submission_time=str,
-                JAT_prio=float,
-                slots=int,              
-                )
+    # conversion of XML fields to Python data
+    CONVERT = defaultdict(
+        # by default, return `str`
+        lambda: str,
+        # return other values in particular cases:
+        JB_job_number=int,
+        JB_submission_time=str,
+        JAT_prio=float,
+        slots=int,          
+        )
 
-        # rename fields to adhere to what the `JobInfo` ctor expects
-        @staticmethod
-        def rename(field):
-                try:
-                        # map field names according to this...
-                        return {
-                                'JB_job_number':'jobid',
-                                'JB_name':'name',
-                                'JB_submission_time':'submit_time',
-                                'JAT_start_time':'start_time',
-                                }[field]
-                except KeyError:
-                        # ...and by default, keep field name unchanged
-                        return field
-
-
-        def __init__(self, dest):
-                self.jobs = dest
-                self.value = [ ]
-                self._level = 0
-
-        def startElement(self,name,attrs):
-                self._level += 1
-                if name == 'job_list':
-                        assert 'state' in attrs
-                        self.current = JobInfo(jobid='invalid', state=JobInfo.OTHER)
-                        self.current_job_state = attrs['state']
-
-                ## for other elements, just reset `value` so we can
-                ## accumulate characters in `self.characters`
-                else:
-                        self.value = []
+    # rename fields to adhere to what the `JobInfo` ctor expects
+    @staticmethod
+    def rename(field):
+        try:
+            # map field names according to this...
+            return {
+                'JB_job_number':'jobid',
+                'JB_name':'name',
+                'JB_submission_time':'submit_time',
+                'JAT_start_time':'start_time',
+                }[field]
+        except KeyError:
+            # ...and by default, keep field name unchanged
+            return field
 
 
-        def characters(self, chrs):
-                self.value.append(chrs)
+    def __init__(self, dest):
+        self.jobs = dest
+        self.value = [ ]
+        self._level = 0
+
+    def startElement(self,name,attrs):
+        self._level += 1
+        if name == 'job_list':
+            assert 'state' in attrs
+            self.current = JobInfo(jobid='invalid', state=JobInfo.OTHER)
+            self.current_job_state = attrs['state']
+
+        ## for other elements, just reset `value` so we can
+        ## accumulate characters in `self.characters`
+        else:
+            self.value = []
 
 
-        def endElement(self,name):
-                self._level -= 1
-                #self.current.submit_time = "1970-01-01T00:00:00"
-                if 0 == self._level:
-                        # end of XML
-                        return
+    def characters(self, chrs):
+        self.value.append(chrs)
 
-                if 'job_list' == name:
-                        # end of job description, commit
-                        self.jobs.append(self.current)
-                        return
 
-                # process job-level elements
-                value_str = str(str.join('', self.value))
-                if 'queue_name' == name:
-                        if '' == value_str:
-                                self.current['queue_name'] = None
-                                self.current['exec_node_name'] = None
-                        else:
-                                self.current['queue_name'] = value_str
-                                # FIXME: GE's queue names have the form queue@hostname;
-                                # raise an appropriate exception if this is not the case!
-                                at = value_str.index('@') + 1
-                                self.current['exec_node_name'] = value_str[at:]
-                elif 'state' == name:
-                        # the GE state letters are explained in the `qstat` man page
-                        if (('E' in value_str)
-                            or ('h' in value_str)
-                            or ('T' in value_str) 
-                            or ('s' in value_str) or ('S' in value_str)
-                            or ('d' in value_str)):
-                                self.current.state = JobInfo.OTHER
-                        elif 'q' in value_str:
-                                self.current.state = JobInfo.PENDING
-                        elif 'r' in value_str:
-                                self.current.state = JobInfo.RUNNING
-                elif 'JB_job_number' == name:
-                        self.current.jobid = value_str
-                elif 'JB_submission_time' == name:
-                        self.current.submitted_at = time.strptime(value_str, '%Y-%m-%dT%H:%M:%S')
-                elif 'JAT_start_time' == name:
-                        self.current.running_at = time.strptime(value_str, '%Y-%m-%dT%H:%M:%S')
-                elif name in self.JOB_ATTRIBUTES:
-                        # convert each XML attribute to a Python representation
-                        # (defaulting to `str`, see CONVERT above)
-                        self.current[self.rename(name)] = self.CONVERT[name](value_str)
+    def endElement(self,name):
+        self._level -= 1
+        #self.current.submit_time = "1970-01-01T00:00:00"
+        if 0 == self._level:
+            # end of XML
+            return
 
-                return
+        if 'job_list' == name:
+            # end of job description, commit
+            self.jobs.append(self.current)
+            return
+
+        # process job-level elements
+        value_str = str(str.join('', self.value))
+        if 'queue_name' == name:
+            if '' == value_str:
+                self.current['queue_name'] = None
+                self.current['exec_node_name'] = None
+            else:
+                self.current['queue_name'] = value_str
+                # FIXME: GE's queue names have the form queue@hostname;
+                # raise an appropriate exception if this is not the case!
+                at = value_str.index('@') + 1
+                self.current['exec_node_name'] = value_str[at:]
+        elif 'state' == name:
+            # the GE state letters are explained in the `qstat` man page
+            if (('E' in value_str)
+                or ('h' in value_str)
+                or ('T' in value_str) 
+                or ('s' in value_str) or ('S' in value_str)
+                or ('d' in value_str)):
+                self.current.state = JobInfo.OTHER
+            elif 'q' in value_str:
+                self.current.state = JobInfo.PENDING
+            elif 'r' in value_str:
+                self.current.state = JobInfo.RUNNING
+        elif 'JB_job_number' == name:
+            self.current.jobid = value_str
+        elif 'JB_submission_time' == name:
+            self.current.submitted_at = time.strptime(value_str, '%Y-%m-%dT%H:%M:%S')
+        elif 'JAT_start_time' == name:
+            self.current.running_at = time.strptime(value_str, '%Y-%m-%dT%H:%M:%S')
+        elif name in self.JOB_ATTRIBUTES:
+            # convert each XML attribute to a Python representation
+            # (defaulting to `str`, see CONVERT above)
+            self.current[self.rename(name)] = self.CONVERT[name](value_str)
+
+        return
 
 
 class GridEngine(BatchSystem):
