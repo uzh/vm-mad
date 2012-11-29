09feb16152098ae1e80efc29d9d98b0d7c461373
riccardo.murri <riccardo.murri@c1fca213-d77e-5eb5-9d24-8ba6f8ca3605>
2012-11-29 13:31:48 +0000

    Add slides presented at the "Cloud-based Service Platforms for the Future Internet" (ICClab @ ZHAW, 29. Nov. 2012)

--- doc/conf.py
+++ doc/conf.py
@@ -113,7 +113,7 @@ release = 'development (SVN $Revision$)'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build', 'html']
+exclude_patterns = ['_build', 'html', 'slides']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
